ed8bd3321dc7df645e82442603bafbd8c434a045
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-09-30 09:50:21 +1300

    player now can't go off the screen

--- Player.cs
+++ Player.cs
@@ -10,8 +10,10 @@ namespace project_2_space_invaders_legin8
 {
     internal class Player
     {
+        private const int SPEEDOFPLAYER = 20;
         private Rectangle formRectangle;
         private PictureBox playerPictureBox;
+
         public Player(Rectangle formRectangle, PictureBox playerPictureBox)
         {
             this.formRectangle = formRectangle;
@@ -21,8 +23,16 @@ namespace project_2_space_invaders_legin8
 
         public void MovePlayer(bool moveLeft)
         {
-            if (moveLeft) playerPictureBox.Left -= 20;
-            if (!moveLeft) playerPictureBox.Left += 20;
+            if (moveLeft)
+            {
+                if (playerPictureBox.Left != formRectangle.Left) playerPictureBox.Left -= SPEEDOFPLAYER;
+                if (playerPictureBox.Left < formRectangle.Left) playerPictureBox.Left = formRectangle.Left;
+            }
+            if (!moveLeft)
+            {
+                if (playerPictureBox.Right != formRectangle.Right) playerPictureBox.Left += SPEEDOFPLAYER;
+                if (playerPictureBox.Right > formRectangle.Right) playerPictureBox.Left = formRectangle.Right - playerPictureBox.Width;
+            }
         }
 
 
