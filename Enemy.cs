f30623d0c77cbc8633bba20d33bee14262646756
legin8 <maynne1@student.op.ac.nz>
2022-10-01 11:20:13 +1300

    Added enemy class, Added list for pictureboxs and const for the gap on the right, started method that will create and place the enemys on the screen

---
+++ Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_2_space_invaders_legin8
+{
+    internal class Enemy
+    {
+        private const int GAPRIGHT = 200;
+        private List<PictureBox> enemies = new List<PictureBox>();
+        private Rectangle formRectangle;
+        public Enemy(Rectangle formRectangle)
+        {
+            this.formRectangle = formRectangle;
+        }
+
+        private void makeEnemy()
+        {
+            int nextPlaceRight = 20;
+            while (nextPlaceRight < formRectangle.Right - GAPRIGHT)
+            {
+                int nextPlaceDown = 20;
+                while (nextPlaceDown < formRectangle.Height)
+                {
+
+                }
+
+            }
+        }
+    }
+}
