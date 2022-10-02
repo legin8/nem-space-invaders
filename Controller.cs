8c3abd1d0d938e61189265b2748df5a043d73eb0
legin8 <maynne1@student.op.ac.nz>
2022-10-02 16:35:08 +1300

    Refactored to create lots of enemys and add to a list in the controller class

--- Controller.cs
+++ Controller.cs
@@ -10,16 +10,52 @@ namespace project_2_space_invaders_legin8
 {
     internal class Controller
     {
+        private const int GAPRIGHT = 200;
         private Player player;
-        private Enemy enemy;
+        //private Enemy enemy;
+        private List<Enemy> enemies = new List<Enemy>();
+        private Form form1;
+        private Rectangle formRectangle;
+        private int spriteSize;
+
 
         public Controller(Rectangle formRectangle, Form form1, int spriteSize)
         {
             player = new Player(formRectangle, form1, spriteSize);
-            enemy = new Enemy(formRectangle, spriteSize, form1);
+            //enemy = new Enemy(formRectangle, spriteSize, form1);
+            this.form1 = form1;
+            this.formRectangle = formRectangle;
+            this.spriteSize = spriteSize;
+            makeEnemy();
         }
 
-        public void MovePlayer(bool moveLeft)
+        private void makeEnemy()
+        {
+            int gap = 20, x = formRectangle.Left + gap, y, index = 0;
+            PictureBox tempPictureBox;
+
+            while (x < formRectangle.Right - GAPRIGHT)
+            {
+
+                y = formRectangle.Top + gap;
+                while (y < formRectangle.Height / 2)
+                {
+                    tempPictureBox = new PictureBox();
+                    tempPictureBox.Width = spriteSize;
+                    tempPictureBox.Height = spriteSize;
+                    tempPictureBox.Image = Properties.Resources.enemy;
+                    tempPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    tempPictureBox.Location = new Point(x, y);
+                    enemies.Add(new Enemy(formRectangle,spriteSize, form1, tempPictureBox));
+                    form1.Controls.Add(tempPictureBox);
+                    index++;
+                    y += spriteSize + gap;
+                }
+                x += spriteSize + gap;
+            }
+        }
+
+            public void MovePlayer(bool moveLeft)
         {
             player.MovePlayer(moveLeft);
         }
--- Enemy.cs
+++ Enemy.cs
@@ -11,42 +11,45 @@ namespace project_2_space_invaders_legin8
 {
     internal class Enemy
     {
-        private const int GAPRIGHT = 200;
-        private List<PictureBox> enemies = new List<PictureBox>();
+        //private const int GAPRIGHT = 200;
+        //private List<PictureBox> enemies = new List<PictureBox>();
         private Rectangle formRectangle;
-        private Form form1;
+        //private Form form1;
         private int spriteSize;
+        private PictureBox pictureBox;
 
-        public Enemy(Rectangle formRectangle, int spriteSize, Form form1)
+        public Enemy(Rectangle formRectangle, int spriteSize, Form form1, PictureBox pictureBox)
         {
             this.formRectangle = formRectangle;
             this.spriteSize = spriteSize;
-            this.form1 = form1;
-            makeEnemy();
+            //this.form1 = form1;
+            //makeEnemy();
+            this.pictureBox = pictureBox;
+            
         }
 
-        private void makeEnemy()
-        {
-            int gap = 20, x = formRectangle.Left + gap, y, index = 0;
+        //private void makeEnemy()
+        //{
+        //    int gap = 20, x = formRectangle.Left + gap, y, index = 0;
 
-            while (x < formRectangle.Right - GAPRIGHT)
-            {
+        //    while (x < formRectangle.Right - GAPRIGHT)
+        //    {
                 
-                y = formRectangle.Top + gap;
-                while (y < formRectangle.Height / 2)
-                {
-                    enemies.Add(new PictureBox());
-                    enemies[index].Width = spriteSize;
-                    enemies[index].Height = spriteSize;
-                    enemies[index].Image = Properties.Resources.enemy;
-                    enemies[index].SizeMode = PictureBoxSizeMode.StretchImage;
-                    enemies[index].Location = new Point(x, y);
-                    form1.Controls.Add(enemies[index]);
-                    index++;
-                    y += spriteSize + gap;
-                }
-                x += spriteSize + gap;
-            }
-        }
+        //        y = formRectangle.Top + gap;
+        //        while (y < formRectangle.Height / 2)
+        //        {
+        //            enemies.Add(new PictureBox());
+        //            enemies[index].Width = spriteSize;
+        //            enemies[index].Height = spriteSize;
+        //            enemies[index].Image = Properties.Resources.enemy;
+        //            enemies[index].SizeMode = PictureBoxSizeMode.StretchImage;
+        //            enemies[index].Location = new Point(x, y);
+        //            form1.Controls.Add(enemies[index]);
+        //            index++;
+        //            y += spriteSize + gap;
+        //        }
+        //        x += spriteSize + gap;
+        //    }
+        //}
     }
 }
