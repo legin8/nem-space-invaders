9e5fa023b8ecef3b971cb7f93062b820d2da33f6
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-10-19 13:13:26 +1300

    Added comments to Form saying what methods do

--- Form1.cs
+++ Form1.cs
@@ -15,10 +15,12 @@ namespace project_2_space_invaders_legin8
 {
     public partial class Form1 : Form
     {
+        // Class variables
         private Controller controller;
         private Random random = new Random();
         private SoundPlayer soundPlayer = new SoundPlayer(@"..\..\Resources\mainGameMusic.wav");
 
+        // Class Constructor
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +28,8 @@ namespace project_2_space_invaders_legin8
             soundPlayer.PlayLooping();
         }
 
+        // Event handler for key input
+        // Moving Left, Right and Shooting
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Left) controller.MovePlayer(true);
@@ -33,11 +37,13 @@ namespace project_2_space_invaders_legin8
             if (e.KeyCode == Keys.Space) controller.Shot();
         }
 
+        // Timer Event handler, Runs the game
         private void timer1_Tick(object sender, EventArgs e)
         {
             controller.RunGame();
         }
 
+        // Event handler for resizing the form
         private void Form1_Resize(object sender, EventArgs e)
         {
             controller.FormRectangle = ClientRectangle;
