a5e4cc4c3b04a54f639de1056387d7f1545c2127
legin8 <maynne1@student.op.ac.nz>
2022-10-20 14:23:54 +1300

    Added sprite class

---
+++ Sprite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_2_space_invaders_legin8
+{
+    public abstract class Sprite
+    {
+        private PictureBox pictureBox;
+        private int spriteSize;
+        public Sprite(int spriteSize)
+        {
+            this.spriteSize = spriteSize;
+        }
+
+    }
+}
