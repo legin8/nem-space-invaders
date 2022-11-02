e6580bd7d46d8ec82b00d094b9a2f2afe91bc889
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-11-02 14:35:39 +1300

    added comments for EDirection and EndGame classes

--- EDirection.cs
+++ EDirection.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Program name: project-2-space-invaders-legin8
+Project file name: EDirection.cs
+Author: Nigel Maynard
+Date: 25/10/22
+Language: C#
+Platform: Microsoft Visual Studio 2022
+Purpose: Class work
+Description: Assessment game: Space Invaders
+Known Bugs:
+Additional Features:
+*/
 
 namespace project_2_space_invaders_legin8
 {
+    // public enum for movement
     public enum EDirection
     {
         DOWN,
--- EndGame.cs
+++ EndGame.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Program name: project-2-space-invaders-legin8
+Project file name: Controller.cs
+Author: Nigel Maynard
+Date: 25/10/22
+Language: C#
+Platform: Microsoft Visual Studio 2022
+Purpose: Class work
+Description: Assessment game: Space Invaders
+Known Bugs:
+Additional Features:
+*/
+
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace project_2_space_invaders_legin8
 {
-    internal class EndGame
+    // This class only sets the background for the form, depending on if you won or not.
+    // Only runs if the player dies or all the enemies are destroyed.
+    public class EndGame
     {
+        // Class variables
         private readonly Bitmap youWin, youLose;
 
+        // Class constructor
         public EndGame(Form form, bool isWinner)
         {
-            youWin = Properties.Resources.youwin;
-            youLose = Properties.Resources.youlose;
-            form.BackgroundImage = isWinner ? youWin : youLose;
+            youWin = Properties.Resources.youwin; // You win picture
+            youLose = Properties.Resources.youlose; // You Lose picture
+            form.BackgroundImage = isWinner ? youWin : youLose; // Sets the background
         }
 
 
