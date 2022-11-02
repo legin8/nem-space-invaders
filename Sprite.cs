6b9a6d062d14e3e92a60d4a48d4905cba3aedf0f
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-11-02 14:16:55 +1300

    commented and did tidy up for Sprite Class

--- Sprite.cs
+++ Sprite.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿/* Program name: project-2-space-invaders-legin8
+Project file name: Sprite.cs
+Author: Nigel Maynard
+Date: 25/10/22
+Language: C#
+Platform: Microsoft Visual Studio 2022
+Purpose: Class work
+Description: Assessment game: Space Invaders
+Known Bugs:
+Additional Features:
+*/
 
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace project_2_space_invaders_legin8
 {
+    // This is the base class of all sprites that will display on the screen
+    // Abstract class
     public abstract class Sprite
     {
+        // protected class variables, not all are assigned here
         protected Bitmap spriteImage;
         protected Form form;
         protected PictureBox spriteBox;
@@ -18,17 +27,23 @@ namespace project_2_space_invaders_legin8
         protected int spriteSize;
         protected EDirection spriteEDirection;
 
+        // Gets and sets
         public EDirection SpriteEDirection { get => spriteEDirection; set => spriteEDirection = value; }
         public PictureBox SpriteBox { get => spriteBox; set => spriteBox = value; }
+
+        // Class constructor
         public Sprite(int spriteSize, Form form, int xPosition, int yPosition)
         {
+            // These are assigned here as they are the same for all children
             this.spriteSize = spriteSize;
             this.form = form;
             formRectangle = form.ClientRectangle;
         }
 
+        // This is the base version for how to make a sprite inside a PictureBox that displays on the screen
         public virtual void MakeSprite(int xPosition, int yPosition)
         {
+            // Creates the PictureBox, sets size, location, picture, SizeMode, adds to the controls to be added to the screen.
             SpriteBox = new PictureBox();
             SpriteBox.Width = spriteSize;
             SpriteBox.Height = spriteSize;
@@ -42,7 +57,7 @@ namespace project_2_space_invaders_legin8
         public abstract void MoveSprite();
 
 
-        // This Removes the sprite from the screen
+        // This Removes the sprite from the screen, aka the Controls and sets the spriteBox to null
         public virtual void RemoveSprite(Sprite sprite)
         {
             form.Controls.Remove(sprite.SpriteBox);
